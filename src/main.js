3b94d68587f0bbc91501037557c02fa6160bec40
Nielsen <asz718460@gmail.com>
2025-05-26 13:49:14 +0800

    chore: 安裝 cropperjs 與 vue3-clipboard 套件

--- src/main.js
+++ src/main.js
@@ -7,11 +7,21 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import '@/assets/tailwind.css'
+import zhTwOverride from './lang/zh-tw'
+import VueClipboard from 'vue3-clipboard'
 
 const app = createApp(App)
+
+app.use(ElementPlus, {
+  locale: zhTwOverride,
+})
 
 app.use(createPinia())
 app.use(router)
 app.use(ElementPlus)
+app.use(VueClipboard, {
+  autoSetContainer: true,
+  appendToBody: true,
+})
 
 app.mount('#app')
